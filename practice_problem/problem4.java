17c196e72901fcddb2fd9a8f9b3c1c389a41b276
unknown <sauravsharma471999@gmail.com>
2025-06-13 09:45:17 +0530

    [Sourav].Add Java fundamental for Day 1

---
+++ practice_problem/problem4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class problem4
+{
+    public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    int rad = sc.nextInt();
+    double area = 3.14 * rad * rad;
+    System.out.print(area);
+}
+}
---
+++ practice_problem/problem9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class problem9
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        double average = (num1 + num2 + num3) / 3;
+        System.out.print(average);
+    }
+}
